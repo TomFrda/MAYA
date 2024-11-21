7c52f12f504a20c366ce54b8d71f5fb050164fe0
Mathieu17M <mathieu.mayard@epitech.eu>
2024-11-21 22:35:37 +0100

    [FIX] ci

--- maya-backend/controllers/conversationController.js
+++ maya-backend/controllers/conversationController.js
@@ -1,4 +1,7 @@
 const Conversation = require('../models/Conversation');
+// const Message = require('../models/Mesage');
+const User = require('../models/User');
+const { io, users } = require('../server'); // Assurez-vous que `io` et `users` sont importés correctement
 
 const getAllConversations = async (req, res) => {
   try {
@@ -13,6 +16,41 @@ const getAllConversations = async (req, res) => {
   }
 };
 
+// const sendMessage = async (req, res) => {
+//     const { to, content } = req.body;
+//     const from = req.user.id;
+  
+//     try {
+//       // Vérifier si l'utilisateur destinataire existe
+//       const recipient = await User.findById(to);
+//       if (!recipient) {
+//         return res.status(404).json({ error: 'Utilisateur destinataire non trouvé' });
+//       }
+  
+//       // Créer et sauvegarder le message
+//       const message = new Message({ from, to, content });
+//       await message.save();
+  
+//       // Ajouter le message à la conversation
+//       const conversation = await Conversation.findOneAndUpdate(
+//         { participants: { $all: [from, to] } },
+//         { $push: { messages: message } },
+//         { new: true, upsert: true }
+//       );
+  
+//       // Envoyer le message via Socket.IO
+//       const recipientSocketId = users[to];
+//       if (recipientSocketId) {
+//         io.to(recipientSocketId).emit('receiveMessage', { from, content });
+//       }
+  
+//       res.status(200).json({ message: 'Message envoyé et stocké avec succès', data: conversation });
+//     } catch (error) {
+//       res.status(500).json({ error: 'Erreur lors de l\'envoi du message', details: error.message });
+//     }
+// };
+
 module.exports = {
-  getAllConversations
+  getAllConversations,
+//   sendMessage
 };
--- maya-backend/routes/conversationRoutes.js
+++ maya-backend/routes/conversationRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getAllConversations } = require('../controllers/conversationController');
+const { getAllConversations, sendMessage } = require('../controllers/conversationController');
 const auth = require('../middleware/auth');
 const router = express.Router();
 
 router.get('/conversations', auth, getAllConversations);
+// router.post('/messages', auth, sendMessage);
 
 module.exports = router;
